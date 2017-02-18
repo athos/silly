d9e004a724f4c6506feae8697d66dcca800aec3d
OHTA Shogo <athos0220@gmail.com>
2017-02-18 22:17:41 +0900

    Implement Silly's core question-answer mechanism

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.395"
-                  :exclusions [org.clojure/tools.reader]]]
+                  :exclusions [org.clojure/tools.reader]]
+                 [cljs-ajax "0.5.8"]]
 
   :plugins [[lein-figwheel "0.5.9"]
             [lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]]
--- src/silly/core.cljs
+++ src/silly/core.cljs
@@ -1,16 +1,74 @@
 (ns silly.core
-  (:require ))
+  (:require-macros [cljs.core.async.macros :refer [go-loop]])
+  (:require [ajax.core :as ajax]
+            [clojure.core.async :as a]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/silly/core.cljs. Go ahead and edit it and see reloading in action.")
+(defn- log [x]
+  (js/console.log x)
+  x)
 
-;; define your app data so that it doesn't get over-written on reload
+(def LUIS_API_URL "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/837d708c-46a1-4612-b750-74b831dd2d94")
+(def LUIS_SUBSCRIPTION_KEY "a5c45cac66734a1d9c34cb4bddd243b8")
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn analyze [message callback]
+  (ajax/GET LUIS_API_URL
+            {:handler callback
+             :response-format :json
+             :params {:subscription-key LUIS_SUBSCRIPTION_KEY
+                      :q message}
+             :keywords? true}))
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn make-answer [result]
+  (let [{:keys [intent score]} (:topScoringIntent result)]
+    (or (when (> score 0.5)
+          (case (keyword intent)
+            :greeting "こんにちは"
+            :ask_weather "天気くらい自分で調べてね"
+            :ask_lookup "それは自分で調べてね"
+            nil))
+        "ちょっと何言ってるか分かりませんね")))
+
+(defn make-recognizer []
+  (let [rec (new js/webkitSpeechRecognition)]
+    (set! (.-lang rec) "ja-JP")
+    (set! (.-interimResults rec) false)
+    rec))
+
+(defn speak [message callback]
+  (let [utterance (new js/SpeechSynthesisUtterance)]
+    (set! (.-lang utterance) "ja-JP")
+    (set! (.-text utterance) message)
+    (.addEventListener utterance "end" callback)
+    (js/speechSynthesis.speak utterance)))
+
+(defn- ->chan [f & args]
+  (let [ch (a/chan)]
+    (apply f (concat args [#(a/put! ch %)]))
+    ch))
+
+(defn main-loop [ch recognizer]
+  (log "main loop started")
+  (go-loop []
+    (log "waiting for message ...")
+    (let [message (log (a/<! ch))
+          answer (make-answer (a/<! (->chan analyze message)))]
+      (a/<! (->chan speak answer))
+      (.start recognizer)
+      (recur))))
+
+(defn -main []
+  (let [ch (a/chan)
+        rec (make-recognizer)]
+    (.addEventListener rec "result"
+      (fn [e]
+        (a/put! ch (-> (.-results e) (aget 0 0) (.-transcript)))))
+    (.addEventListener rec "end" #(.stop rec))
+
+    (main-loop ch rec)
+    (.start rec)))
+
+(.addEventListener js/window "load" -main)
+
+(defn on-js-reload [])
